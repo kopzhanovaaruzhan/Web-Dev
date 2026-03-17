1a0864511e691e64cd2391557820ea6edbbe4018
kopzhanovaaruzhan <a_kopzhanova@kbtu.kz>
2026-03-17 16:18:52 +0500

    HackerRank

---
+++ lab7/task1/hackerrank/2.py
@@ -0,0 +1,10 @@
+#Print a function
+
+if __name__ == '__main__':
+    n = int(input())
+    string=''
+    for i in range (1, n+1):
+        pat=str(i)
+        string+=pat
+        
+    print(string)
---
+++ lab7/task1/hackerrank/7.py
@@ -0,0 +1,8 @@
+#Division
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a//b)
+    print(a/b)
